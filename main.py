8746a82957da3946883778143b1d42e96bd02388
barbara bb <sbustamantebarbara@gmail.com>
2025-06-18 22:01:05 -0400

    feat/barbara creo funcion eliminar contacto

---
+++ main.py
@@ -0,0 +1,15 @@
+def eliminar_contacto(contactos):
+    print("\n--- Eliminar Contacto ---")
+    if not contactos:
+        print("No hay contactos para eliminar.")
+        return
+    nombre_eliminar = input("Ingrese el nombre del contacto a eliminar: ").lower()
+    cantidad_inicial = len(contactos)
+    # Se crea una nueva lista excluyendo los contactos a eliminar
+    contactos[:] = [contacto for contacto in contactos if nombre_eliminar not in contacto['nombre'].lower()]
+    if len(contactos) < cantidad_inicial:
+        print("Contacto(s) eliminado(s) exitosamente.")
+    else:
+        print("No se encontró ningún contacto con ese nombre para eliminar.")
+
+    
